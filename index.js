25e4a76b804cf04d1885317366e9f808477d2e41
Marcin Tomaszewski <marcin.tomaszewski7@wp.pl>
2018-09-24 09:16:42 +0200

    Change the path to the directory.

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ var fs = require('fs');
 var colors = require('colors');
 
 function readAndWriteContent() {
-    fs.readdir('../NODE-badamy-otoczenie-praca-z-plikami', 'utf-8', function(err, data) {
-        fs.writeFile('./text.txt', data, function (err){
+    fs.readdir('./', 'utf-8', function(err, data) {       //sprawdza zawartość katalogu
+        fs.writeFile('./text.txt', data, function (err){        //writeFile() zastępuje  lub tworzy nowy plik jeśli podany nie istnieje
             if(err) throw err;
             console.log('The file has been save.'.rainbow);
-            //console.log(data);
         });
     });
 }
